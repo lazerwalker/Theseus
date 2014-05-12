26bb46e42aefbaa409141cd9a8289df1e6e103bd
Mike Walker <michael@lazerwalker.com>
2014-05-12 12:02:39 -0400

    Initial commit

---
+++ OpenData/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenData
+//
+//  Created by Michael Walker on 5/12/14.
+//  Copyright (c) 2014 Lazer-Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OpenData/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OpenData
+//
+//  Created by Michael Walker on 5/12/14.
+//  Copyright (c) 2014 Lazer-Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
